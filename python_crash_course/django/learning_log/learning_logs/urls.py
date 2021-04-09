557f57dd8da6c8f0c14871b01337f3a8272ac6b9
Tony Held <tony.held@gmail.com>
2021-04-09 14:21:16 -0700

    added trydjango tutorial

--- python_crash_course/django/learning_log/learning_logs/urls.py
+++ python_crash_course/django/learning_log/learning_logs/urls.py
@@ -11,4 +11,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page that shows topics
     path('topics/', views.topics, name='topics'),
+    # Details for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page ofr adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
 ]
