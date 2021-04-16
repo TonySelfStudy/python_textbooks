1a4508923172701f5cace0277a161c4c2fcce14d
Tony Held <tony.held@gmail.com>
2021-04-16 15:17:42 -0700

    Learning Log Complete! Next up, learn to deploy

--- python_crash_course/django/learning_log2/learning_logs/forms.py
+++ python_crash_course/django/learning_log2/learning_logs/forms.py
@@ -6,17 +6,20 @@ class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
-        labels = {'text': 'text label'}
+        labels = {'text': ''}
+        widgets = {'text': forms.TextInput(attrs={'autofocus': 'autofocus',
+                                                  'placeholder': 'New Topic Name',
+                                                  })}
 
 
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['topic', 'text', ]
-        labels = {'topic': 'select topic',
-                  'text': 'your entry',
+        labels = {'topic': 'Select Topic',
+                  'text': 'Your Entry',
                   }
         widgets = {'text': forms.Textarea(
             attrs={'cols': 80,
-                   'placeholder': 'Enter entry information here',
+                   'placeholder': 'Enter your entry information here ...',
                    'autofocus': 'autofocus'})}
